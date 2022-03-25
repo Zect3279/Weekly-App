3858f62a90879394188425b82022d53a1704130e
Zect <zeck.3279@gmail.com>
2022-03-25 20:33:42 +0900

    Copy 003

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,29 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
       <router-view/>
     </v-main>
+  <v-footer
+    color="primary lighten-1"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+        @click="jump(link)"
+      >
+        {{ link }}
+      </v-btn>
+    </v-row>
+  </v-footer>
   </v-app>
 </template>
 
@@ -49,7 +33,28 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
-  })
+    links: [
+      'Home',
+      'Count',
+      'Load'
+    ]
+  }),
+  methods: {
+    jump (to) {
+      switch (to) {
+        case 'Home':
+          this.$router.push('/')
+          break
+        case 'Count':
+          this.$router.push('/count')
+          break
+        case 'Load':
+          this.$router.push('/load')
+          break
+        default:
+          break
+      }
+    }
+  }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '@/views/Home.vue'
+import Counter from '@/views/Counter.vue'
+import Load from '@/views/Load.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/count',
+    name: 'Counter',
+    component: Counter
+  },
+  {
+    path: '/load',
+    name: 'Load',
+    component: Load
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
